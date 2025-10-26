2126b4d626cd2d3b7563a04d3288707ccc7b190d
Ayoob <ayoobajward@gmail.com>
2025-10-26 20:39:34 +1100

    2.11 done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,17 +1,19 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './Filter'
 import Form from './Form'
 import List from './List'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [searchTerm, setSearchTerm] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons').then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   return (
     <div>
@@ -20,11 +22,6 @@ const App = () => {
       <h2>Add a new</h2>
       <Form persons={persons} setPersons={setPersons} />
       <h2>Numbers</h2>
-      {/*<div>
-        {persons
-          .filter(person => person.name.toLowerCase().match(searchTerm))
-          .map(person => <div key={person.id} >{person.name} {person.number}</div>)}
-      </div>*/}
       <List searchTerm={searchTerm} persons={persons} />
     </div>
   )
