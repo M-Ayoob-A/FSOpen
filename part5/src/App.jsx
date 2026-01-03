73387a27f7d858cabe0972ac93504e47c5081fe5
Ayoob <ayoobajward@gmail.com>
2026-01-03 15:48:15 +1100

    5.4 done

--- part5/src/App.jsx
+++ part5/src/App.jsx
@@ -1,9 +1,14 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import Notif from './components/Notif'
+import Error from './components/Error'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
 const App = () => {
+  
+  const [notifMessage, setNotifMessage] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
   const [blogs, setBlogs] = useState([])
   
   /////////////////////////////// LOGIN THINGS
@@ -21,15 +26,15 @@ const App = () => {
       setUsername('')
       setPassword('')
     } catch {
-      /*setErrorMessage('wrong credentials')
+      setErrorMessage('wrong username or password')
       setTimeout(() => {
         setErrorMessage(null)
-      }, 5000)*/
-      console.log('login error')
+      }, 5000)
     }
   }
 
-  const handleLogOut = async () => {
+  const handleLogOut = async (event) => {
+    event.preventDefault()
     console.log('logging out', username, password)
     window.localStorage.removeItem('blogsAppUser')
     setUser(null)
@@ -40,7 +45,7 @@ const App = () => {
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
       setUser(user)
-      //blogService.setToken(user.token)
+      blogService.setToken(user.token)
     }
   }, [])
 
@@ -75,6 +80,37 @@ const App = () => {
 
   /////////////////////////////// LOGIN THINGS
 
+  const [title, setTitle] = useState('') 
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
+  const handleCreate = async event => {
+    event.preventDefault()
+    
+    const newBlog = {
+      title: title,
+      author: author,
+      url: url
+    }
+
+    try {
+      const createdBlog = await blogService.createNew(newBlog)
+      setBlogs(blogs.concat(createdBlog))
+      //console.log(blogs)
+      
+      setNotifMessage(`a new blog ${title} by ${author}`)
+      setTimeout(() => {
+        setNotifMessage(null)
+      }, 5000)
+    } catch {
+      setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+    
+  }
+
   const blogList = () => (
     <div>
       <h2>blogs</h2>
@@ -84,22 +120,66 @@ const App = () => {
     </div>
   )
 
+  const createBlog = () => (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={handleCreate}>
+        <div>
+          <label>
+            title
+            <input
+              type="text"
+              value={title}
+              onChange={({ target }) => setTitle(target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            author
+            <input
+              type="text"
+              value={author}
+              onChange={({ target }) => setAuthor(target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            url
+            <input
+              type="text"
+              value={url}
+              onChange={({ target }) => setUrl(target.value)}
+            />
+          </label>
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+    blogService.getAll().then(blogs => {
+      setBlogs(blogs)
+    })
   }, [])
 
   return (
     <>
+      <Notif message={notifMessage} />
+      <Error message={errorMessage} />
       {!user && loginForm()}
+      <div>
       {user && (
         <div>
           <p>{user.name} logged in</p>
           <button onClick={handleLogOut}>logout</button>
+          {createBlog()}
           {blogList()}
         </div>
       )}
+      </div>
     </>
   )
 }
--- part5/src/services/blogs.js
+++ part5/src/services/blogs.js
@@ -2,9 +2,29 @@ import axios from 'axios'
 const baseUrl = '/api/blogs'
 //const baseUrl = 'http://localhost:3001/api/persons'
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+let token = null
+
+const setToken = newToken => {
+  token = 'Bearer ' + newToken
 }
 
-export default { getAll }
+/*const getAll = async () => {
+  const res = await axios.get(baseUrl)
+  return res.data
+}*/
+
+const getAll = () => {
+  const res = axios.get(baseUrl)
+  return res.then(response => response.data)
+}
+
+const createNew = async newBlog => {
+  const config = {
+    headers: { Authorization: token }
+  }
+
+  const res = await axios.post(baseUrl, newBlog, config)
+  return res.data
+}
+
+export default { getAll, createNew, setToken }
